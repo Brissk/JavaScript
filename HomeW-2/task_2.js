c30f6f0508499d6b1cb3e18784e8a0e784f4003e
«Mikle <cobra-mikle@mail.ru>
2023-06-16 13:21:06 +0300

    hw2

---
+++ HomeW-2/task_2.js
@@ -0,0 +1,14 @@
+/* 
+Задание 2
+Перепишите код к тернарному оператору
+
+let test = true;
+if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}
+*/
+
+let test = false;
+console.log((test === true) ? "+++" : "---");
