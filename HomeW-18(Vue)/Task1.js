655601842f02ab872275940605c8ed389caa10c0
«Mikle <cobra-mikle@mail.ru>
2023-09-02 19:22:10 +0300

    hw18

---
+++ HomeW-18(Vue)/Task1.js
@@ -0,0 +1,43 @@
+/*
+Задание 1
+Вы разрабатываете приложение для отображения и сортировки списка товаров. У вас есть массив объектов products, где каждый объект представляет товар с его названием и ценой. Вам необходимо отобразить список товаров и предоставить пользователю возможность сортировать товары по цене (по возрастанию и по убыванию).
+
+*/
+
+const HelloVueApp = {
+    data() {
+        return {
+            products: [
+                {
+                    id: 1,
+                    name: "Iphone",
+                    price: 100000
+                },
+                {
+                    id: 2,
+                    name: "Xiaomi",
+                    price: 10000
+                },
+                {
+                    id: 3,
+                    name: "Samsung",
+                    price: 30000
+                },
+                {
+                    id: 4,
+                    name: "Asus",
+                    price: 20000
+                }
+            ]
+        }
+    },
+    methods: {
+        productAscending() {
+            this.products.sort((a, b) => a.price > b.price ? 1 : -1);
+        }
+    }
+}
+
+
+Vue.createApp(HelloVueApp).mount('#hello-vue')
+
